fc6a0e1f9daef4b3803c9b90f84467a127e7ced0
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-29 13:58:10 +0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({ quiet: true, path: "./config/.env" });
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
