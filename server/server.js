c2b9f6eab4846feb2d92c8f2d09980f211947cac
KhasanMeliev <khasanmeliev2@gmail.com>
2026-01-30 17:10:28 +0500

    backend is done

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ dotenv.config({ quiet: true, path: "./.env" });
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongoDB.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
 
 app.listen(PORT, () => {
   connectDB();
